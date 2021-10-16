f4f4620d1d748bb04d579946d64bd8a48759a6a3
cleilson pereira <marcialwushu@hotmail.com>
2021-10-16 19:40:22 -0300

    :fire: Adicionado Entidades

---
+++ src/MeusProdutos/DevIO.Business/Core/Models/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Core.Models
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+        
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/MeusProdutos/DevIO.Business/Models/Fornecedores/Fornecedor.cs
@@ -0,0 +1,13 @@
+﻿using DevIO.Business.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Fornecedores
+{
+    public class Fornecedor : Entity
+    {
+    }
+}
